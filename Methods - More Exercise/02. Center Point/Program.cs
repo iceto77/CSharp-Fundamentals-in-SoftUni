638e468fe40a9663acae3df92857484ecba7e77f
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-06-21 00:47:12 +0300

    Add files via upload

---
+++ Methods - More Exercise/02. Center Point/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double coordinateX1 = double.Parse(Console.ReadLine());
+            double coordinateY1 = double.Parse(Console.ReadLine());
+            double coordinateX2 = double.Parse(Console.ReadLine());
+            double coordinateY2 = double.Parse(Console.ReadLine());
+            double point1 = FindPoint(Math.Abs(coordinateX1), Math.Abs(coordinateY1));
+            double point2 = FindPoint(Math.Abs(coordinateX2), Math.Abs(coordinateY2));
+            if (point1 > point2)
+            {
+                Console.WriteLine($"({coordinateX2}, {coordinateY2})");
+            }
+            else
+            {
+                Console.WriteLine($"({coordinateX1}, {coordinateY1})");
+            }
+        }
+
+        static double FindPoint(double xCoordinate, double yCoordinate)
+        {
+            double result = Math.Pow(xCoordinate, 2) + Math.Pow(yCoordinate, 2);
+            return Math.Sqrt(result);
+        }
+    }
+}
---
+++ Methods - More Exercise/03. Longer Line/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Longer_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputs = new double[8];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                inputs[i] = double.Parse(Console.ReadLine());
+            }
+
+            double line1 = FindPoint(Math.Abs(inputs[0]) + Math.Abs(inputs[2]), Math.Abs(inputs[1]) + Math.Abs(inputs[3]));
+            double line2 = FindPoint(Math.Abs(inputs[4]) + Math.Abs(inputs[6]), Math.Abs(inputs[5]) + Math.Abs(inputs[7]));
+            if (line1 >= line2)
+            {
+                double point1 = FindPoint(Math.Abs(inputs[0]), Math.Abs(inputs[1]));
+                double point2 = FindPoint(Math.Abs(inputs[2]), Math.Abs(inputs[3]));
+                if (point1 > point2)
+                {
+                    Console.WriteLine($"({inputs[2]}, {inputs[3]})({inputs[0]}, {inputs[1]})"); ;
+                }
+                else
+                {
+                    Console.WriteLine($"({inputs[0]}, {inputs[1]})({inputs[2]}, {inputs[3]})");
+                }
+            }
+            else
+            {
+                double point1 = FindPoint(Math.Abs(inputs[4]), Math.Abs(inputs[5]));
+                double point2 = FindPoint(Math.Abs(inputs[6]), Math.Abs(inputs[7]));
+                if (point1 > point2)
+                {
+                    Console.WriteLine($"({inputs[6]}, {inputs[7]})({inputs[4]}, {inputs[5]})"); ;
+                }
+                else
+                {
+                    Console.WriteLine($"({inputs[4]}, {inputs[5]})({inputs[6]}, {inputs[7]})");
+                }
+            }
+        }
+
+        static double FindPoint(double xCoordinate, double yCoordinate)
+        {
+            double result = Math.Pow(xCoordinate, 2) + Math.Pow(yCoordinate, 2);
+            return Math.Sqrt(result);
+        }
+    }
+}
