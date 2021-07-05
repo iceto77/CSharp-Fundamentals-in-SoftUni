3f0ab7018b83d0abcc6643422aae3403cb85028b
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-07-05 23:14:18 +0300

    Add files via upload

---
+++ Programming Fundamentals Mid Exam/01. Counter Strike/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string inputComand = Console.ReadLine();
+            int winCount = 0;
+            bool win = true;
+            while (inputComand != "End of battle")
+            {
+                int distance = int.Parse(inputComand);
+                if (distance > energy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {winCount} won battles and {energy} energy");
+                    win = false;
+                    inputComand = "End of battle";
+                }
+                else
+                {
+                    energy -= distance;
+                    winCount++;
+                    if (winCount % 3 == 0)
+                    {
+                        energy += winCount;
+                    }
+                    inputComand = Console.ReadLine();
+                }
+            }
+            if (win)
+            {
+            Console.WriteLine($"Won battles: {winCount}. Energy left: {energy}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Mid Exam/02. Shoot for the Win/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Shoot_for_the_Win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split()
+                            .Select(int.Parse).ToArray();
+            string inputCommand = Console.ReadLine();
+            while (inputCommand != "End")
+            {
+                int indexOfTarget = int.Parse(inputCommand);
+                if (indexOfTarget < targets.Length)
+                {
+                    int curentValue = targets[indexOfTarget];
+                    if (targets[indexOfTarget] != -1)
+                    {
+                        targets[indexOfTarget] = -1;
+                    }
+                    if (curentValue != -1)
+                    {
+                        for (int i = 0; i < targets.Length; i++)
+                        {
+                            if (targets[i] != -1)
+                            {
+                                if (targets[i] > curentValue)
+                                {
+                                    targets[i] -= curentValue;
+                                }
+                                else
+                                {
+                                    targets[i] += curentValue;
+                                }
+                            }
+                        }
+                    }
+                }
+                inputCommand = Console.ReadLine();
+            }
+            int countOfShotTargets = 0;
+            for (int i = 0; i < targets.Length; i++)
+            {
+                if (targets[i] == -1)
+                {
+                    countOfShotTargets++;
+                }
+            }
+            Console.Write($"Shot targets: {countOfShotTargets} -> ");
+            Console.WriteLine(string.Join(" ", targets));
+        }
+    }
+}
