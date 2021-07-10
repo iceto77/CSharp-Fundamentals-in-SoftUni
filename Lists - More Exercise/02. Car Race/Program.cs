2f64fd2f9227d61a27588fc318faba9df230dac7
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-07-10 21:43:21 +0300

    Add files via upload

---
+++ Lists - More Exercise/02. Car Race/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger[] carRace = Console.ReadLine().Split()
+                .Select(BigInteger.Parse).ToArray();
+            BigInteger midle = carRace.Length / 2;
+            double leftSum = 0;
+            double rightSum = 0;
+            for (int i = 0; i < midle; i++)
+            {
+                leftSum += (double)carRace[i];
+                if (carRace[i] == 0)
+                {
+                    leftSum *= 0.8d;
+                }
+            }
+            for (int i = carRace.Length - 1; i > midle; i--)
+            {
+                rightSum += (double)carRace[i];
+                if (carRace[i] == 0)
+                {
+                    rightSum *= 0.8d;
+                }
+            }
+            if (leftSum < rightSum)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftSum}");
+            }
+            else if (leftSum > rightSum)
+            {
+                Console.WriteLine($"The winner is right with total time: {rightSum}");
+            }
+        }
+    }
+}
