ce9f142e069c9df1dbd0761c863167de1cd5e0b9
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-08-02 00:31:05 +0300

    Add files via upload

---
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"^[>]{2}(?<name>[A-Za-z]+)[<]{2}(?<price>[0-9]+[.]*[0-9]*)[!](?<qnt>[0-9]+)";
+            double totalMoney = 0;
+            Console.WriteLine("Bought furniture:");
+            while (input != "Purchase")
+            {
+                MatchCollection result = Regex.Matches(input, pattern);
+                foreach (Match item in result)
+                {
+                    Console.WriteLine(item.Groups["name"].Value);
+                    double price = double.Parse(item.Groups["price"].Value);
+                    double quantity = double.Parse(item.Groups["qnt"].Value);
+                    totalMoney += price * quantity;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {totalMoney:f2}");
+        }
+    }
+}
---
+++ Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string namePattern = @"[A-Za-z]";
+            string distancePattern = @"[0-9]";
+            string[] racersName = input.Split(", ");
+            Dictionary<string, int> racers = new Dictionary<string, int>();
+            for (int i = 0; i < racersName.Length; i++)
+            {
+                if (!racers.ContainsKey(racersName[i]))
+                {
+                    racers.Add(racersName[i], 0);
+                }
+            }
+            input = Console.ReadLine();
+            while (input != "end of race")
+            {
+                MatchCollection curentName = Regex.Matches(input, namePattern);
+                string name = string.Empty;
+                foreach (var stepName in curentName)
+                {
+                    name += stepName;
+                }
+                if (racers.ContainsKey(name))
+                {
+                    MatchCollection curentDistance = Regex.Matches(input, distancePattern);
+                    int distance = 0;
+                    foreach (var stepDistance in curentDistance)
+                    {
+                        distance += int.Parse(stepDistance.ToString());
+                    }
+                    racers[name] += distance;
+                }
+                input = Console.ReadLine();
+            }
+            int posCount = 1;
+            foreach (var item in racers.OrderByDescending(x => x.Value))
+            {
+                string position = string.Empty;
+                switch (posCount)
+                {
+                    case 1:
+                        position = "1st";
+                        break;
+                    case 2:
+                        position = "2nd";
+                        break;
+                    case 3:
+                        position = "3rd";
+                        break;
+                }
+                Console.WriteLine($"{position} place: {item.Key}");
+                posCount++;
+                if (posCount > 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
