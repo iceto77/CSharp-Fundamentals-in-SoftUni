84b076f53718972afff6fe6d27630072b6a6df92
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-08-08 10:09:06 +0300

    Add files via upload

---
+++ Programming Fundamentals Final Exam/01. The Imitation Game/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string incomingMessage = Console.ReadLine();
+            StringBuilder decodingMessage = new StringBuilder(incomingMessage);
+            incomingMessage = Console.ReadLine();
+            while (incomingMessage != "Decode")
+            {
+                string[] command = incomingMessage.Split("|").ToArray();
+                switch (command[0])    
+                {
+                    case "Move":
+                        int moves = int.Parse(command[1]);
+                        StringBuilder temText = new StringBuilder();
+                        for (int i = 0; i < moves; i++)
+                        {
+                            temText.Append(decodingMessage[i]);
+                        }
+                        decodingMessage.Append(temText);
+                        temText.Clear();
+                        for (int i = moves; i < decodingMessage.Length; i++)
+                        {
+                            temText.Append(decodingMessage[i]);
+                        }
+                        decodingMessage.Clear();
+                        decodingMessage.Append(temText);
+                        break;
+                    case "Insert":
+                        int index = int.Parse(command[1]);
+                        string value = command[2];
+                        decodingMessage.Insert(index, value);
+                        break;
+                    case "ChangeAll":
+                        string target = command[1];
+                        string newText = command[2];
+                        while (decodingMessage.ToString().Contains(target))
+                        {
+                            decodingMessage.Replace(target, newText);
+                        }
+                        break;
+                }
+                incomingMessage = Console.ReadLine();
+            }
+            Console.WriteLine($"The decrypted message is: {decodingMessage}");
+        }
+    }
+}
---
+++ Programming Fundamentals Final Exam/03. The Pianist/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._The_Pianist
+{
+    class Piece
+    {
+        public Piece(string composer, string key)
+        {
+            Composer = composer;
+            Key = key;
+        }
+        public string Composer { get; set; }
+        public string Key { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int songNum = int.Parse(Console.ReadLine());
+            SortedDictionary<string, Piece> piecesData = new SortedDictionary<string, Piece>();
+            string input = string.Empty;
+            for (int i = 0; i < songNum; i++)
+            {
+                input = Console.ReadLine();
+                string[] currentPiece = input.Split("|", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                piecesData.Add(currentPiece[0], new Piece(currentPiece[1], currentPiece[2]));
+            }
+            input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                string[] command = input.Split("|", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                switch (command[0])
+                {
+                    case "Add":
+                        if (!piecesData.ContainsKey(command[1]))
+                        {
+                            piecesData.Add(command[1], new Piece(command[2], command[3]));
+                            Console.WriteLine($"{command[1]} by {command[2]} in {command[3]} added to the collection!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{command[1]} is already in the collection!");
+                        }
+                        break;
+                    case "Remove":
+                        if (piecesData.ContainsKey(command[1]))
+                        {
+                            piecesData.Remove(command[1]);
+                            Console.WriteLine($"Successfully removed {command[1]}!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid operation! {command[1]} does not exist in the collection.");
+                        }
+                            break;
+                    case "ChangeKey":
+                        if (piecesData.ContainsKey(command[1]))
+                        {
+                            piecesData[command[1]].Key = command[2];
+                            Console.WriteLine($"Changed the key of {command[1]} to {command[2]}!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid operation! {command[1]} does not exist in the collection.");
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in piecesData.OrderBy(x => x.Key).ThenBy(x => x.Value.Composer))
+            {
+                Console.WriteLine($"{item.Key} -> Composer: {item.Value.Composer}, Key: {item.Value.Key}");
+            }
+        }
+    }
+}
