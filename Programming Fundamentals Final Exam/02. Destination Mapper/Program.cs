0add8c47df0e55624d4f383095d0a2d22e9b7498
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-08-08 13:54:20 +0300

    Add files via upload

---
+++ Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=\/]{1})(?<name>[A-Z][A-Za-z]{2,})\1";
+            MatchCollection validLocations = Regex.Matches(input, pattern);
+            int travelPoints = 0;
+            List<string> destinations = new List<string>();
+            foreach (Match item in validLocations)
+            {
+                if (!destinations.Contains(item.Groups["name"].Value))
+                {
+                    destinations.Add(item.Groups["name"].Value);
+                }
+            }
+            foreach (var item in destinations)
+            {
+                travelPoints += item.Length;
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
