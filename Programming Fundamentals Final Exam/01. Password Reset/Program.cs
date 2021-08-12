4b63c6a6ebb54179f82d0d25bb6368ccc27dd24e
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-08-12 18:54:01 +0300

    Add files via upload

---
+++ Programming Fundamentals Final Exam/01. Password Reset/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder myPassword = new StringBuilder(input);
+            input = Console.ReadLine();
+            while (input != "Done")
+            {
+                string[] command = input.Split().ToArray();
+                StringBuilder newPass = new StringBuilder();
+                switch (command[0])
+                {
+                    case "TakeOdd":
+                        string temp = myPassword.ToString();
+                        newPass.Clear();
+                        for (int i = 1; i < temp.Length; i += 2)
+                        {
+                            newPass.Append((char)temp[i]);
+                        }
+                        myPassword = newPass;
+                        Console.WriteLine(myPassword.ToString());
+                        break;
+                    case "Cut":
+                        int index = int.Parse(command[1]);
+                        int length = int.Parse(command[2]);
+                        temp = myPassword.ToString();
+                        string targetSubstring = temp.Substring(index, length);
+                        int startIndex = temp.IndexOf(targetSubstring);
+                        newPass.Clear();
+                        for (int i = 0; i < startIndex; i++)
+                        {
+                            newPass.Append((char)temp[i]);
+                        }
+                        for (int i = startIndex + length; i < temp.Length; i++)
+                        {
+                            newPass.Append((char)temp[i]);
+                        }
+                        myPassword = newPass;
+                        Console.WriteLine(myPassword.ToString());
+                        break;
+                    case "Substitute":
+                        temp = myPassword.ToString();
+                        if (temp.Contains(command[1]))
+                        {
+                            myPassword.Replace(command[1], command[2]);
+                            Console.WriteLine(myPassword.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {myPassword.ToString()}");
+        }
+    }
+}
