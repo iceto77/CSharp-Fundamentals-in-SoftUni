c08ac68ccf6a4f914c4d7da5d673ccc5e2b61a3f
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-08-13 20:10:44 +0300

    Add files via upload

---
+++ Programming Fundamentals Final Exam/01. Activation Keys/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder rawActivationKey = new StringBuilder(input);
+            input = Console.ReadLine();
+            while (input != "Generate")
+            {
+                string[] command = input.Split(">>>").ToArray();
+                switch (command[0])
+                {
+                    case "Contains":
+                        string substring = command[1];
+                        if (rawActivationKey.ToString().Contains(substring))
+                        {
+                            Console.WriteLine($"{rawActivationKey} contains {substring}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Substring not found!");
+                        }
+                        break;
+                    case "Flip":
+                        int startIndex = int.Parse(command[2]);
+                        int endIndex = int.Parse(command[3]);
+                        bool IsUpper = (command[1] == "Upper");
+                        for (int i = startIndex; i < endIndex; i++)
+                        {
+                            if (IsUpper)
+                            {
+                                if (rawActivationKey[i] > 96 && rawActivationKey[i] < 123)
+                                {
+                                    char newChar = (char)(rawActivationKey[i] - 32);
+                                    rawActivationKey[i] = newChar;
+                                }
+                            }
+                            else
+                            {
+                                if (rawActivationKey[i] > 64 && rawActivationKey[i] < 91)
+                                {
+                                    char newChar = (char)(rawActivationKey[i] + 32);
+                                    rawActivationKey[i] = newChar;
+                                }
+                            }
+                        }
+                        Console.WriteLine(rawActivationKey);
+                        break;
+                    case "Slice":
+                        startIndex = int.Parse(command[1]);
+                        int lenght = int.Parse(command[2]) - startIndex;
+                        rawActivationKey.Remove(startIndex, lenght);
+                        Console.WriteLine(rawActivationKey);
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Your activation key is: {rawActivationKey}");
+        }
+    }
+}
---
+++ Programming Fundamentals Final Exam/02. Emoji Detector/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordPattern = @"([:*])\1([A-Z][a-z]{2,})\1{2}";
+            string digitPattern = @"([0-9])";
+            string input = Console.ReadLine();
+            MatchCollection findCoolThreshold = Regex.Matches(input, digitPattern);
+            BigInteger coolThreshold = 1;
+
+            foreach (Match item in findCoolThreshold)
+            {
+                coolThreshold *= int.Parse(item.Value);
+            }
+            MatchCollection findEmoji = Regex.Matches(input, wordPattern);
+            int findedEmoji = findEmoji.Count;
+            Dictionary<string, BigInteger> CoolEmoji = new Dictionary<string, BigInteger>();
+            foreach (Match emoji in findEmoji)
+            {
+                string curentEmoji = emoji.Groups[2].Value;
+                int curentCoolness = 0;
+                for (int i = 0; i < curentEmoji.Length; i++)
+                {
+                    curentCoolness += curentEmoji[i];
+                }
+                if (curentCoolness > coolThreshold)
+                {
+                    CoolEmoji.Add(emoji.Value, curentCoolness);
+                }
+            }
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{findedEmoji} emojis found in the text. The cool ones are:");
+            if (CoolEmoji.Count > 0)
+            {
+                foreach (var item in CoolEmoji)
+                {
+                    Console.WriteLine($"{item.Key} ");
+                }
+            }
+        }
+    }
+}
